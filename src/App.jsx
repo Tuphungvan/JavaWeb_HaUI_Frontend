31b1563f5d7c072f525caa89cb86bf7b063a36be
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2025-11-11 22:17:15 +0700

    base

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable */
+import React, { useEffect } from "react";
+import { useRoutes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Toaster } from "react-hot-toast";
+import { ToastContainer } from "react-toastify";
+
+const App = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Thời gian hiệu ứng (ms)
+      once: true, // Chỉ chạy một lần khi cuộn
+    });
+  }, []);
+
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+  return (
+    <>
+      <ToastContainer />
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 980,
+        }}
+        limit={10}
+      />
+      {routes}
+    </>
+  );
+};
+
+export default App;
